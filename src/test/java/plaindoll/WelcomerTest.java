fa8c48f3c98dc69cea72f50ed5e10ef2a7108383
OshchepkovDP <kajitsydmitry@gmail.com>
2026-03-03 14:52:32 +0300

    Add getHunterReply method and corresponding test2

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -6,36 +6,39 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class WelcomerTest {
-	
-	private Welcomer welcomer = new Welcomer();
 
-        @Test
-        public void testGetHunterReplyContainsHunter() {
-                Welcomer welcomer = new Welcomer();
-                String reply = welcomer.getHunterReply();
-                assertTrue("Reply should contain 'hunter'", reply.toLowerCase().contains("hunter"));
-        }
+    private Welcomer welcomer = new Welcomer();
 
-	@Test
-	public void welcomerSaysWelcome() {
-		assertThat(welcomer.sayWelcome(), containsString("Welcome"));
-	}
+    @Test
+    public void testGetHunterReplyContainsHunter() {
+        String reply = welcomer.getHunterReply();
+        assertNotNull("Reply should not be null", reply);
+        assertTrue("Reply should contain 'hunter'", reply.toLowerCase().contains("hunter"));
+    }
 
-	@Test
-	public void welcomerSaysFarewell() {
-		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
-	}
-	@Test
-	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
-	}
-	@Test
-	public void welcomerSaysSilver(){
-		assertThat(welcomer.sayNeedGold(), containsString("gold"));
-	}
-	@Test
-	public void welcomerSaysSomething(){
-		assertThat(welcomer.saySome(), containsString("something"));
-	}
+    @Test
+    public void welcomerSaysWelcome() {
+        assertThat(welcomer.sayWelcome(), containsString("Welcome"));
+    }
+
+    @Test
+    public void welcomerSaysFarewell() {
+        assertThat(welcomer.sayFarewell(), containsString("Farewell"));
+    }
+
+    @Test
+    public void sayWelcomeAndFarewellContainHunter() {
+        assertThat(welcomer.sayWelcome(), containsString("hunter"));
+        assertThat(welcomer.sayFarewell(), containsString("hunter"));
+    }
+
+    @Test
+    public void welcomerSaysSilver() {
+        assertThat(welcomer.sayNeedGold(), containsString("gold"));
+    }
+
+    @Test
+    public void welcomerSaysSomething() {
+        assertThat(welcomer.saySome(), containsString("something"));
+    }
 }
